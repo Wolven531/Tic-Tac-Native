7b4e8e15ea52c9c186ef050cce93e0f9c4a531af
Anthony Williams <anthony.williams.cs@gmail.com>
2019-03-07 23:12:06 -0700

    Use new Grid component

--- App.js
+++ App.js
@@ -1,76 +1,25 @@
 import React from 'react'
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
+import { Grid } from './Grid'
+
 export default class App extends React.Component {
 	render() {
 		return (
 			<View style={styles.container}>
 				<Text>Tic Tac Toe</Text>
-				<View style={styles.gridStyle}>
-					<View style={styles.rowStyle}>
-						<TouchableOpacity style={styles.cell}>
-							<Text>X</Text>
-						</TouchableOpacity>
-						<TouchableOpacity style={styles.cell}>
-							<Text>X</Text>
-						</TouchableOpacity>
-						<TouchableOpacity style={styles.cell}>
-							<Text>X</Text>
-						</TouchableOpacity>
-					</View>
-					<View style={styles.rowStyle}>
-						<TouchableOpacity style={styles.cell}>
-							<Text>O</Text>
-						</TouchableOpacity>
-						<TouchableOpacity style={styles.cell}>
-							<Text>O</Text>
-						</TouchableOpacity>
-						<TouchableOpacity style={styles.cell}>
-							<Text>O</Text>
-						</TouchableOpacity>
-					</View>
-					<View style={styles.rowStyle}>
-						<TouchableOpacity style={styles.cell}>
-							<Text>X</Text>
-						</TouchableOpacity>
-						<TouchableOpacity style={styles.cell}>
-							<Text>X</Text>
-						</TouchableOpacity>
-						<TouchableOpacity style={styles.cell}>
-							<Text>X</Text>
-						</TouchableOpacity>
-					</View>
-				</View>
+				<Grid />
 			</View>
 		)
 	}
 }
 
 const styles = StyleSheet.create({
-	cell: {
-		alignItems: 'center',
-		backgroundColor: 'rgba(0,255,0,.7)',
-		borderColor: '#000',
-		borderStyle: 'solid',
-		borderWidth: 1,
-		height: 75,
-		justifyContent: 'center',
-		width: '33.33%'
-	},
 	container: {
 		alignItems: 'center',
 		backgroundColor: '#fff',
 		flex: 1,
 		flexDirection: 'column',
 		justifyContent: 'center'
-	},
-	gridStyle: {
-		backgroundColor: 'rgba(0,200,200,.5)',
-		marginTop: 20,
-		width: '80%'
-	},
-	rowStyle: {
-		display: 'flex',
-		flexDirection: 'row'
 	}
 })
