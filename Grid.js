710f7cec70ea23989c0f77e9ce81bcba3e5d929c
Anthony Williams <anthony.williams.cs@gmail.com>
2019-03-10 21:15:25 -0600

    Use new stateless Cell in Grid

--- Grid.js
+++ Grid.js
@@ -4,22 +4,38 @@ import { StyleSheet, View } from 'react-native'
 import { Cell } from './Cell'
 
 class Grid extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			gridRows: [
+				['X','','X'],
+				['','X',''],
+				['X','','X']
+			]
+		}
+	}
+
 	render() {
-		const gridRows = [
-			['X','X','X'],
-			['O','O','O'],
-			['X','X','X']
-		]
+		const { gridRows } = this.state
 
 		return (
 			<View style={styles.gridStyle}>
 				{gridRows.map((row, rowInd) =>
 					<View style={styles.rowStyle} key={`row${rowInd}`}>
 						{row.map((cell, cellInd) =>
-							<Cell key={`cell${cellInd}`} display={cell} />)}
+							<Cell key={`cell${cellInd}`}
+								display={cell}
+								onCellPress={newValue => this.updateBoardCell(rowInd, cellInd, newValue)} />)}
 					</View>)}
 			</View>
 		)
+	}
+
+	updateBoardCell = (row, col, newVal) => {
+		const updatedGrid = [...this.state.gridRows]
+		updatedGrid[row][col] = newVal
+		this.setState({ gridRows: updatedGrid })
 	}
 }
 
