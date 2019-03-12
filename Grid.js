8b077034a0ced49649d655ff81d6d27add6a384b
Anthony Williams <anthony.williams.cs@gmail.com>
2019-03-11 21:29:37 -0600

    Make Grid stateless

--- Grid.js
+++ Grid.js
@@ -4,43 +4,55 @@ import { StyleSheet, View } from 'react-native'
 import { Cell } from './Cell'
 
 class Grid extends React.Component {
-	constructor(props) {
-		super(props)
-
-		this.state = {
-			gridRows: [
-				['X','','X'],
-				['','O',''],
-				['X','','X']
-			]
-		}
-	}
-
 	render() {
-		const { gridRows } = this.state
+		const { gridRows } = this.props
 
 		return (
 			<View style={styles.gridStyle}>
-				{gridRows.map((row, rowInd) =>
+				{gridRows.map((row, rowInd) => (
 					<View style={styles.rowStyle} key={`row${rowInd}`}>
-						{row.map((cell, cellInd) =>
-							<Cell key={`cell${cellInd}`}
+						{row.map((cell, cellInd) => (
+							<Cell
+								key={`cell${cellInd}`}
 								display={cell}
-								onCellPress={newValue => this.updateBoardCell(rowInd, cellInd, newValue)} />)}
-					</View>)}
+								onCellPress={newValue =>
+									this.updateBoardCell(
+										rowInd,
+										cellInd,
+										newValue
+									)
+								}
+							/>
+						))}
+					</View>
+				))}
 			</View>
 		)
 	}
 
 	updateBoardCell = (row, col, newVal) => {
 		const updatedGrid = [...this.state.gridRows]
-		if (updatedGrid[row] === undefined || updatedGrid[row][col] === undefined) {
+		if (updatedGrid[row] === undefined ||
+			updatedGrid[row][col] === undefined) {
 			console.warn(`Invalid cell supplied: row=${row} col=${col} newVal="${newVal}"`)
 			return
 		}
 		updatedGrid[row][col] = newVal
 		this.setState({ gridRows: updatedGrid })
 	}
+}
+
+/*
+	NOTE: structure of gridRows is array of 3 arrays each containing 3 strings
+	Example:
+		gridRows: [
+			['X','','X'],
+			['','O',''],
+			['X','','X']
+		]
+*/
+Grid.propTypes = {
+	gridRows: PropTypes.arrayOf(PropTypes.array).isRequired
 }
 
 const styles = StyleSheet.create({
