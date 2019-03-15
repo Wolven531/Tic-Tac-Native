6b317264736c28f52ee2716e4c6db5dfad92a307
Anthony Williams <anthony.williams.cs@gmail.com>
2019-03-14 21:34:09 -0600

    Change winner algo to return winner

--- App.js
+++ App.js
@@ -16,34 +16,65 @@ export default class App extends React.Component {
 	}
 
 	render() {
+		const winner = this.checkForBoardWin()
+		const winnerDisplay = winner === '' ? 'Board has no winner' : `Winner: ${winner}`
+
 		return (
 			<View style={styles.container}>
 				<Text>Tic Tac Toe</Text>
-				<Text>Board Has Winner: {this.boardHasWinner() ? 'True' : 'False'}</Text>
+				<Text>{winnerDisplay}</Text>
 				<Grid gridRows={this.state.gridRows} onGridUpdated={this.onGridUpdated} />
 			</View>
 		)
 	}
 
-	rowHasWinner = row => {
+	checkForBoardWin = () => {
+		const { gridRows } = this.state
+
+		let result = this.checkForRowWin([ gridRows[0][0], gridRows[0][1], gridRows[0][2] ])
+		if (result !== '') {
+			return result
+		}
+		result = this.checkForRowWin([ gridRows[1][0], gridRows[1][1], gridRows[1][2] ])
+		if (result !== '') {
+			return result
+		}
+		result = this.checkForRowWin([ gridRows[2][0], gridRows[2][1], gridRows[2][2] ])
+		if (result !== '') {
+			return result
+		}
+		result = this.checkForRowWin([ gridRows[0][0], gridRows[1][0], gridRows[2][0] ])
+		if (result !== '') {
+			return result
+		}
+		result = this.checkForRowWin([ gridRows[0][1], gridRows[1][1], gridRows[2][1] ])
+		if (result !== '') {
+			return result
+		}
+		result = this.checkForRowWin([ gridRows[0][2], gridRows[1][2], gridRows[2][2] ])
+		if (result !== '') {
+			return result
+		}
+		result = this.checkForRowWin([ gridRows[0][0], gridRows[1][1], gridRows[2][2] ])
+		if (result !== '') {
+			return result
+		}
+		result = this.checkForRowWin([ gridRows[2][0], gridRows[1][1], gridRows[0][2] ])
+		if (result !== '') {
+			return result
+		}
+		return ''
+	}
+
+	checkForRowWin = row => {
 		const leftCol = row[0]
 		const middleCol = row[1]
 		const rightCol = row[2]
 
-		return leftCol !== '' && (leftCol === middleCol && middleCol === rightCol)
-	}
-
-	boardHasWinner = () => {
-		const { gridRows } = this.state
-
-		return this.rowHasWinner([ gridRows[0][0], gridRows[0][1], gridRows[0][2] ])
-			|| this.rowHasWinner([ gridRows[1][0], gridRows[1][1], gridRows[1][2] ])
-			|| this.rowHasWinner([ gridRows[2][0], gridRows[2][1], gridRows[2][2] ])
-			|| this.rowHasWinner([ gridRows[0][0], gridRows[1][0], gridRows[2][0] ])
-			|| this.rowHasWinner([ gridRows[0][1], gridRows[1][1], gridRows[2][1] ])
-			|| this.rowHasWinner([ gridRows[0][2], gridRows[1][2], gridRows[2][2] ])
-			|| this.rowHasWinner([ gridRows[0][0], gridRows[1][1], gridRows[2][2] ])
-			|| this.rowHasWinner([ gridRows[2][0], gridRows[1][1], gridRows[0][2] ])
+		if(leftCol !== '' && (leftCol === middleCol && middleCol === rightCol)) {
+			return leftCol
+		}
+		return ''
 	}
 
 	onGridUpdated = updatedGrid => this.setState({gridRows: updatedGrid})
